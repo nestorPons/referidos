362e7c5005491833745906b43a9a7e62f7021114
nestorpons <nestorpons@gmail.com>
2022-07-26 23:54:28 +0200

    Actualizada plantilla y conectado con los repositorios

--- app/config/routes.php
+++ app/config/routes.php
@@ -1,22 +1,22 @@
 <?php namespace FOLDER;
 
 // Carpetas
-const PUBLIC_FOLDER_ROOT = 'htdocs/';
+const PUBLIC_FOLDER = 'htdocs/';
 const APP = ROOT . 'app/'; 
 const SRC = ROOT . 'src/';
-const PUBLIC_FOLDER = 'www/'; 
 
 const CORE          = APP . 'core/';
 const CONFIG        = APP . 'config/';
 const DB            = APP . 'db/';
 const CACHES        = APP . 'caches/';
 const HELPERS       = APP . 'helpers/';
+const LIBS          = APP . 'libs/';
 const MODELS        = SRC . 'models/';
 const CONTROLLERS   = SRC . 'controllers/';
 const VIEWS         = SRC . 'views/';
 const JS            = SRC . 'js/';
 const IMG           = SRC . 'img/';
-const COMPONENTS  = SRC . 'components/';
+const COMPONENTS    = SRC . 'components/';
 const STYLES        = SRC . 'styles/';
 
 
